b157960afb246502dac718b26bea7535939dfed1
Leonardo Perez <A01665462@tec.mx>
2025-05-07 12:23:23 -0600

    Sidebar para linkedin y github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/Navbar"
+import Sidebar from "@/components/Sidebar"
 export default function Home() {
   return (
     <div className="bg-pizarra h-full mt-28">
       <Navbar /> {/*Ignorar esto*/}
+      <Sidebar />
       <div >
         fortnite
       </div>
