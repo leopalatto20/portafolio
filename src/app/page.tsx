e5664db7383bfb274af230a1012efc5b1850b2a4
Leonardo Perez <A01665462@tec.mx>
2025-05-07 13:10:29 -0600

    Agregar foto y gradiente al fondo de nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
-    <div className="bg-pizarra h-full mt-28">
+    <div className="bg-pizarra h-full pt-28">
       <Navbar /> {/*Ignorar esto*/}
       <Sidebar />
-      <div>fortnite</div>
+      <main className="flex min-h-screen min-w-full flex-col items-center justify-center">
+        <section id="about" className="flex justify-center w-3/4 gap-16">
+          <div>fortnite</div>
+          <div>forntite2</div>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,15 @@ import "@/styles/Navbar.css";
 
 export default function Navbar() {
   return (
-    <div className="fixed top-0 left-0 right-0 w-full backdrop-blur-md bg-pizarra/80">
+    <div className="fixed top-0 left-0 right-0 w-full backdrop-blur-md bg-pizarra/80 bg-linear-to-b from-sky-900 to-pizarra">
       <div className="flex w-4/5 mx-auto items-center justify-between p-4 rounded-2xl ">
         <Image
           id="image"
-          src="/images.png"
+          src="/gato.jpeg"
           alt="fortnite"
           width={75}
           height={75}
+          className="rounded-full"
         />
         <nav id="navbar" className="flex gap-4">
           <a className="nav-text" href="#about">
