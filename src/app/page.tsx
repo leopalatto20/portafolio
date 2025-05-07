41ce076e7de78981ddc34842a9156fcdda10a5e1
Leonardo Perez <A01665462@tec.mx>
2025-05-07 11:09:09 -0600

    Cambiar estilos navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
+import Navbar from "@/components/Navbar"
 export default function Home() {
   return (
-    <div>
-      fortnite
+    <div className="bg-pizarra">
+      <Navbar />
+      <div>
+        fortnite
+      </div>
     </div>
 
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,27 @@
 import Image from 'next/image'
+import "@/styles/Navbar.css"
 
 export default function Navbar() {
-  const textProps = 'text-gray-500 transition-all duration-300 hover:scale-105 hover:text-black hover:ease-in-out'
   return (
-    <div className="flex w-4/5 mx-auto items-center justify-between">
+    <div className="flex w-4/5 mx-auto items-center justify-between p-4 rounded-2xl">
       <Image
+        id='image'
         src="/images.png"
         alt="fortnite"
-        width={50}
-        height={50}
+        width={75}
+        height={75}
       />
-      <nav id="navbar" className="bg-white">
-        <a className={textProps} href='#about'>
+      <nav id="navbar" className="flex gap-4">
+        <a className="nav-text" href='#about'>
           Sobre mi
         </a>
-        <a className={textProps} href='#about'>
+        <a className="nav-text" href='#about'>
           Tech stack
         </a>
-        <a className={textProps} href='#about'>
+        <a className="nav-text" href='#about'>
           Proyectos
         </a>
-        <a className={textProps} href='#about'>
+        <a className="nav-text" href='#about'>
           Contacto
         </a>
       </nav>
