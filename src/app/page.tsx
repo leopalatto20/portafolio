7b261b5ca0dcb9a37687aab76e3aef1094b70f5f
Leonardo Perez <A01665462@tec.mx>
2025-05-07 13:59:23 -0600

    Gradient en texto, poner descripcion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,31 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
-    <div className="bg-pizarra h-full pt-28">
+    <div className="bg-pizarra h-full ">
       <Navbar /> {/*Ignorar esto*/}
       <Sidebar />
       <main className="flex min-h-screen min-w-full flex-col items-center justify-center">
-        <section id="about" className="flex justify-center w-3/4 gap-16">
-          <div>fortnite</div>
-          <div>forntite2</div>
+        <section
+          id="about"
+          className="text-center grid md:grid-cols-2 grid-cols-1 items-center w-3/4 "
+        >
+          <div className="flex flex-col items-start">
+            <h1 className="text-3xl">Hola, soy</h1>
+            <h1 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-miel to-yellow-400">
+              Leonardo Palatto
+            </h1>
+          </div>
+          <div>
+            <button className="relative border-2 border-miel bg-transparent text-miel text-2xl rounded-2xl p-5 hover:bg-miel hover:text-black hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg overflow-hidden ">
+              <a
+                href="https://drive.google.com/file/d/1AO4ORIn8ag3t5IfbKIehQ3z2S5q6sO4l/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Curriculum Vitae
+              </a>
+            </button>
+          </div>
         </section>
       </main>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import "@/styles/Navbar.css";
 
 export default function Navbar() {
   return (
-    <div className="fixed top-0 left-0 right-0 w-full backdrop-blur-md bg-pizarra/80 bg-linear-to-b from-sky-900 to-pizarra">
+    <div className="fixed top-0 left-0 right-0 w-full backdrop-blur-md bg-pizarra/80 bg-linear-to-b from-black/20 to-pizarra">
       <div className="flex w-4/5 mx-auto items-center justify-between p-4 rounded-2xl ">
         <Image
           id="image"
@@ -13,7 +13,7 @@ export default function Navbar() {
           height={75}
           className="rounded-full"
         />
-        <nav id="navbar" className="flex gap-4">
+        <nav id="navbar" className="flex gap-5">
           <a className="nav-text" href="#about">
             Sobre mi
           </a>
